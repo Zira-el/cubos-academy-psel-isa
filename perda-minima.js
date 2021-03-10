be9e4245109720ce621481d51e132ccaa6b533ed
zira-ell <niltonmenezes@live.com>
2021-03-09 22:39:02 -0300

    first commit

---
+++ perda-minima.js
@@ -0,0 +1,18 @@
+function solucao(precos) {
+    // seu código aqui
+
+    let a = 0;
+
+    for (count = 0; count < precos.length; count++) {
+        for (aux = count + 1; aux < precos.length; aux++) {
+
+            if (precos[count] - precos[aux] < a && precos[count] - precos[aux] > 0 || a === 0) {
+                a = precos[count] - precos[aux];
+                if (a < 0) {
+                    a = 0;
+                }
+            }
+        }
+    }
+    console.log(a);
+}
